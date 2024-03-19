f63ed84eb7f1346c3920f3e837ba0d8d42a789c6
FlacSy <flacsy.x@gmail.com>
2024-03-19 14:18:24 +0100

    🚫 Fix bug with file uploading

--- main.py
+++ main.py
@@ -20,20 +20,19 @@ async def register_routers():
 async def load_modules(directories, ignore_files = ["__init__.py"]):
     for directory in directories:
         for filename in os.listdir(directory):
-            if filename.endswith(".py") and filename not in ignore_files:
+            if str(filename).endswith(".py") and filename not in ignore_files:
                 module_name = os.path.splitext(filename)[0]
                 spec = importlib.util.spec_from_file_location(module_name, os.path.join(directory, filename))
                 module = importlib.util.module_from_spec(spec)
                 spec.loader.exec_module(module)
 
-
 async def main():
     print('\033[32mБот запущен!\033[39m')
     bot = Bot(TOKEN, parse_mode=ParseMode.MARKDOWN)
     await register_routers()
-    await dp.start_polling(bot)
-    dirs = [r"bot\user\handlers\commands", r"bot\user\handlers\masseges", r"bot\user\handlers\callbacks"]  
+    dirs = ["./bot/user/handlers/commands", "./bot/user/handlers/messages", "./bot/user/handlers/callbacks"]  
     await load_modules(dirs)
+    await dp.start_polling(bot)
     
 if __name__ == "__main__":
     logger = Logger()
